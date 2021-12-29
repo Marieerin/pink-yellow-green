17224e92c4bc3b098f7423f3362bd46201a7afc4
Marieerin <m.oshaughnessy@outlook.com>
2021-12-29 10:25:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Shape from './Shape'
 import './App.css';
 
 function App() {
+  const [currentWord, setCurrentWord] = useState({
+    hex: '#72d979',
+    name: 'pink'
+  });
+
+  const [colorArr, setColorArr] = useState([{hex: '#ff33d1', name: 'pink'}, {hex: '#ffe633', name: 'yellow'}, {hex: '#72d979', name: 'green'}]);
+
+  const colors = {
+    red: '#ff3333',
+    orange: '#ff7433',
+    yellow: '#ffe633',
+    green: '#72d979',
+    blue: '#3386ff',
+    purple: '#8a33ff',
+    pink: '#ff33d1'
+  };
+
+  const keys = Object.keys(colors);
+
+  const colorRandomizer = () => {
+    const color0 = keys[Math.floor(Math.random() * keys.length)];
+    const color1 = keys[Math.floor(Math.random() * keys.length)];
+    const color2 = keys[Math.floor(Math.random() * keys.length)];
+    const colorArray =[{hex: colors[color0], name: color0}, {hex: colors[color1], name: color1}, {hex: colors[color2], name: color2}];
+    const randomName = colorArray[Math.floor(Math.random() * 2)];
+    const randomHex = colorArray[Math.floor(Math.random() * 2)];
+
+
+    setColorArr(colorArray);
+    setCurrentWord({
+        hex: `${randomHex.hex}`,
+        name: `${randomName.name}`
+      });
+  };
+
+  const shapeClicker = (color) => {
+    console.log(color, currentWord.name);
+    if (color === currentWord.name){
+      colorRandomizer();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wordBox">
+        <span style={{color:`${currentWord.hex}`}}>{currentWord.name}</span>
+      </div>
+      <div id='shapeBox'>
+        {colorArr.map((shapeColor, i)=> {
+          return (
+            <Shape color={shapeColor.hex} name={shapeColor.name} key={i} shapeClicker={shapeClicker}  />
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Shape.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+import './App.css';
+
+function Shape(props) {
+    const style = {
+      background: props.color,
+    };
+
+    const placeHolder = () => {
+        props.shapeClicker(props.name)
+    };
+
+    return (
+        <div className='shape' style={style} onClick={placeHolder} ></div>
+    )
+}
+
+export default Shape;
