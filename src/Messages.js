2e68767d50fc5055e85f2305ebddc2896269b3b2
Marieerin <m.oshaughnessy@outlook.com>
2022-01-02 17:36:27 -0500

    tidying up code

--- src/Messages.js
+++ src/Messages.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 
 const Messages = (props) => {
     const [displayMessages, setDisplayMessages] = useState([]);
@@ -6,55 +6,27 @@ const Messages = (props) => {
     const loss = props.loss;
     const total = props.total;
 
-    const tenWins = `You got 10 right!! Can you make it to 100?`
-    const tenLoss = `Oh no! You got 10 wrong!`
-
-    let w = 0
-    let l = 0
+    const tenWins = `You got 10 right!! Can you make it to 100?`;
+    const tenLoss = `Oh no! You got 10 wrong!`;
+    const hundredWin = `WOW look at you! You got to 100!`
 
     document.addEventListener('click', () => {
-        // if(displayMessages.includes(tenWins) && wins === 1) {
-        //     console.log('yes');
-        // }
-        
-        if(!displayMessages.includes(tenWins) && wins === 1){    
-            w = w + 1
+        if(!displayMessages.includes(tenWins) && wins === 10){    
             setDisplayMessages([...displayMessages, tenWins]);
-        } else if (!displayMessages.includes(tenLoss)&& loss === 1) {
-            l = l + 1
-            setDisplayMessages([...displayMessages, tenLoss]);
         } 
-        console.log(displayMessages.find(o => o.name === `${tenWins}`));
+        else if (!displayMessages.includes(tenLoss) && loss === 10) {
+            setDisplayMessages([...displayMessages, tenLoss]);
+        } else if (!displayMessages.includes(hundredWin) && wins === 100) {
+            setDisplayMessages([...displayMessages, hundredWin]);
+        }
     });
-
-    // const messageTrigger = () => {
-    //     // let w = 0
-    //     // let l = 0
-    //     // let t = 0
-    //     // console.log(displayMessages);
-    //     const dM = displayMessages
-
-    //         if(wins === 1){
-    //             w = w + 1
-    
-    //             console.log(w );
-    //             // setDisplayMessages([...dM, tenWins]);
-                
-    
-    //     } else if (loss === 1) {
-    //         // displayMessages.push(tenLoss)
-    //         console.log(displayMessages, "loss");
-    //     } else if (w === 0) {
-    //         return
-    //     }
-    // }
 
     return (
         <div id='messages'>
             {displayMessages.map((message, i) => {
                 console.log(message);
                 return(
-                    <p key={i}>
+                    <p key={i} className='messageBox' >
                         {message}
                     </p>
                 )
